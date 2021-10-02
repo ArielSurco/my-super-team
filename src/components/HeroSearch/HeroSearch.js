ad41a5341632581e8f7f5e4a92aa497f4cc6f0c8
Ariel Surco Acho <arielsurcoa@gmail.com>
2021-10-02 16:27:13 -0300

    Add searchHero function

--- src/components/HeroSearch/HeroSearch.js
+++ src/components/HeroSearch/HeroSearch.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import { useFormik } from 'formik';
 import './HeroSearch.css';
 
-const HeroSearch = () => {
+const HeroSearch = ( {searchHero} ) => {
+    const formik = useFormik({
+        initialValues: {
+            searchValue: '',
+        },
+        onSubmit: value => {
+            searchHero(value)
+        }
+    })
+
     return (
-        <form action="">
+        <form action="" onSubmit={formik.handleSubmit}>
             <div className="form-group d-flex justify-content-center">
                 <input 
                 type="text" 
                 className="search-input d-inline-block form-control fs-2" 
-                id="search"
-                name="search"
-                placeholder="Search"/>
+                id="searchValue"
+                name="searchValue"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.searchValue}
+                placeholder="Search"
+                />
                 <button 
                 type="submit"
                 className="btn btn-primary search-button">
--- src/routes/Search/Search.js
+++ src/routes/Search/Search.js
@@ -1,19 +1,46 @@
 import React from 'react';
 import { HeroSearch } from '../../components/HeroSearch/HeroSearch';
-import { useSearchHero } from '../../hooks/useSearchHero';
-// import { HeroCard } from '../../components/HeroCard/HeroCard';
-
+import axios from 'axios';
+import { HeroCard } from '../../components/HeroCard/HeroCard';
 import './Search.css';
 
+const searchHero = async (valueSearched) => {
+    try {
+        const response = await axios.get(`https://www.superheroapi.com/api.php/1972691609555893/search/${valueSearched}`)
+            .then(res => res.data);
+        return response
+    } catch(error) {
+        console.error(new Error(error))
+    }
+}
+
 const Search = () => {
-    const [heroes, searchHero] = useSearchHero('')
-    
+    const [heroes, setHeroes] = React.useState({});
+    const [isLoading, setIsLoading] = React.useState(true);
     const isXSViewport = window.innerWidth < 576
+    React.useEffect(() => {
+        const loadHeroes = async () => {
+            try {
+                const response = await searchHero('bat');
+                setHeroes(response.results);
+                setIsLoading(false);
+            } catch(error) {
+                console.error(new Error(error));
+            }
+        }
+        loadHeroes();
+    }, [])
 
     return (
         <div className="container search-route">
-            <HeroSearch/>
+            <HeroSearch searchHero={searchHero} />
             <div className={`mt-5 row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-4 justify-content-${isXSViewport ? "center" : "start"}`}>
+                {isLoading ? null : heroes.map(hero => (
+                    <HeroCard
+                    heroInfo={hero}
+                    key={hero.id}
+                    />
+                ))}
             </div>
         </div>
     )
