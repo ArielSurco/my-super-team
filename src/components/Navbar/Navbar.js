7a7feb1c9c5022f1e284a0b9678e363925c709ab
ArielSurco <arielsurcoa@gmail.com>
2021-10-10 21:29:28 -0300

    Remove hamburguer menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,20 +12,12 @@ const Navbar = () => {
             <Link className="navbar-brand logo" to="/">
                 MST
             </Link>
-            <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navMenu" aria-controls="navMenu">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse navMenu" id="navMenu">
-                {/* <Link className="btn btn-outline-success login-btn" to="/login">
-                    Logout
-                </Link> */}
-                <ul className="navbar-nav ms-3">
-                    {session.isAuth ? (
-                    <li className="nav-item" onClick={logout}>
-                        <Link className="nav-link" to="/login">Logout</Link>
-                    </li>) : null}
-                </ul>
-            </div>
+            <ul className="navbar-nav ms-3">
+                {session.isAuth && (
+                    <li onClick={logout}>
+                    <Link className="nav-link" to="/login">Log out</Link>
+                </li>)}
+            </ul>
         </div>
     </nav>
     )
