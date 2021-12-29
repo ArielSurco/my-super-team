d9cf0196d8755dbae619eb0bdfccc9da7fae740d
ArielSurco <arielsurcoa@gmail.com>
2021-12-29 17:44:37 -0300

    Allow any email and password temporarily

--- src/hooks/useUserState.js
+++ src/hooks/useUserState.js
@@ -10,23 +10,33 @@ const useUserState = () => {
     const [session, saveSession] = useLocalStorage('session', initialSession);
 
     const baseURL = process.env.REACT_APP_BASE_URL;
-    const login = async (loginData) => {
-        try {
-            const response = await axios.post(baseURL, loginData)
-            .then( res => res.data );
-            if(!response.error)
-            {
-                addAlert("Successful login", 'success')
-                saveSession({
-                    token: response.token,
-                    isAuth: true
-                });
-                window.location.pathname="/"
-            }
-        } catch(error) {
-            addAlert("Wrong email or password", 'error');
-        }
+    
+    const login = loginData => {
+        addAlert("Successful login", 'success');
+        saveSession({
+            token:'1',
+            isAuth: true
+        })
+        window.location.pathname="/"
     }
+    //The request does not work because the API uses the HTTP protocol (Should be HTTPS)
+    // const login = async (loginData) => {
+    //     try {
+    //         const response = await axios.post(baseURL, loginData)
+    //         .then( res => res.data );
+    //         if(!response.error)
+    //         {
+    //             addAlert("Successful login", 'success')
+    //             saveSession({
+    //                 token: response.token,
+    //                 isAuth: true
+    //             });
+    //             window.location.pathname="/"
+    //         }
+    //     } catch(error) {
+    //         addAlert("Wrong email or password", 'error');
+    //     }
+    // }
 
     const logout = () => {
         saveSession({token:'', isAuth: false});
